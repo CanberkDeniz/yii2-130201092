80f145ad4ce9644b0253b2faa92e06ac895b40dc
CanberkDeniz <gipssy95@gmail.com>
2021-01-17 07:16:02 +0000

    module initial

---
+++ src/controllers/DefaultController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace CanberkDeniz\players\controllers;
+
+class DefaultController extends \yii\web\Controller
+{
+public function actionIndex()
+{
+return $this->render('index');
+}
+}
---
+++ src/module.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace CanberkDeniz\players;
+
+class Module extends \yii\base\Module
+{
+    public function init()
+{
+parent::init();
+}
+
+}
